19633fa3eacb0cf7b890838dd5b13987e9e30223
nurislammridha <mridha851@gmail.com>
2025-09-16 14:05:15 +0600

    complete auth..

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Sanctum Middleware for API authentication
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\RegistrationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::apiResource('tasks', TaskController::class);
+// Route::apiResource('tasks', TaskController::class);
+
+Route::post('/register', [RegistrationController::class, 'register']);
+Route::post('/verify-otp', [RegistrationController::class, 'verifyOtp']);
+Route::post('/login', [RegistrationController::class, 'login']);
+Route::post('/forgot-password', [RegistrationController::class, 'forgotPassword']);
+Route::post('/reset-password', [RegistrationController::class, 'resetPassword']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+});
